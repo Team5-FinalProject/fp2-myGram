deee357dd868f6ebabc0a803edbf6cc676c4d27f
alfathajirizkyanto <alfathajirizkyanto@gmail.com>
2023-05-12 21:10:27 +0700

    add user data in getSocialMedia & fix create user data order

--- models/socialmedia.js
+++ models/socialmedia.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.User, { foreignKey: "UserId", as: "user" });
+      this.belongsTo(models.User);
     }
   }
   Socialmedia.init(
