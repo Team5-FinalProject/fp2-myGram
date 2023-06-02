170d898dbcfa54f53c0a8ac5f1959382d3c9fc28
alfathajirizkyanto <alfathajirizkyanto@gmail.com>
2023-06-02 12:11:56 +0700

    create authorization for socialMedia routes

--- routers/socialmediaRouters.js
+++ routers/socialmediaRouters.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 const SocialMediaController = require("../controllers/socialmediaController");
+const authentication = require("../middlewares/authentication");
+const authorization = require("../middlewares/socialMedia_authorization");
 
+router.use(authentication);
 router.post("/", SocialMediaController.createSocialmedia);
 router.get("/", SocialMediaController.getAllSocialmedias);
+router.use("/:id", authorization);
 router.put("/:id", SocialMediaController.updateSocialmediaById);
 router.delete("/:id", SocialMediaController.deleteSocialmediaById);
 
